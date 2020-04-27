b9d12c6ecbe4b6bc850c03322aa2e620d65baea8
yujunjiex <15070329437@163.com>
2020-04-27 17:16:57 +0800

    Add a generic model of slider captcha.

---
+++ slider_captcha/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir weights && cd weights
+
+# Download weights for vanilla YOLOv3
+wget -c https://pjreddie.com/media/files/yolov3.weights
+# # Download weights for tiny YOLOv3
+wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
+# Download weights for backbone network
+wget -c https://pjreddie.com/media/files/darknet53.conv.74
