91846c7b69fbc50a273743f8692d79d05ce7c021
yujunjiex <15070329437@163.com>
2020-04-28 14:06:10 +0800

    yunpian yolov3 net finished.

---
+++ slider_captcha/yunpian/detect.sh
@@ -0,0 +1,1 @@
+python3 ../detect.py --model_def config/yolov3-captcha.cfg --weights_path checkpoints/yolov3_ckpt.pth --image_folder data/test_images --class_path data/classes.names
---
+++ slider_captcha/yunpian/train.sh
@@ -0,0 +1,1 @@
+python3 ../train.py --model_def config/yolov3-captcha.cfg --data_config config/captcha.data --pretrained_weights ../weights/darknet53.conv.74
